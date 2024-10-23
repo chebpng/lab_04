f0a5b2ee2d6f5c838ab93a81f17d10e2f52c011b
chebpng <yota3301@gmail.com>
2024-10-23 21:49:14 +0700

    Add counting func for 11 dot

--- 2.2/backup/unit1.pas
+++ 2.2/backup/unit1.pas
@@ -5,10 +5,19 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Label1: TLabel;
   private
 
   public
---
+++ 2.2/ну эта хуйня считает на 11 точек.pas
@@ -0,0 +1,21 @@
+﻿
+var
+x, y, min, max, x_len:real;
+i:integer;
+
+Begin
+writeln('Введи начало оси x');
+readln(min);
+writeln('Введи конец оси x');
+readln(max);
+//похуй пусть 11 точек будет
+x_len := (abs(min) + abs(max));
+x_len := (x_len/11);
+x := min;
+for i:= 1 to 11 do
+  begin   
+    y:= (x*x+2);
+    writeln(x, ' | ', y);
+    x := x + x_len;
+  end;
+end.
