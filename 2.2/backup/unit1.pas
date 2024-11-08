b05ae97b3f710b2b9ba9241ac548da1745511718
chebpng <yota3301@gmail.com>
2024-11-09 00:08:33 +0700

    Finish

--- 2.2/backup/unit1.pas
+++ 2.2/backup/unit1.pas
@@ -35,56 +35,64 @@ end;
 //конвертация между системами кординат
 procedure convert;
 begin
-ix := Trunc(Round((((x+10)*(600-50))/(10+10))+50));
-jy := Trunc(Round((((y-10)*(600-100))/(10+10))+600));
+ix := Trunc(Round((((x+10)*(form1.image1.width-0))/(10+10))+0));
+jy := Trunc(Round((((y-10)*(form1.image1.Height -0))/(10+10))+form1.image1.Height ));
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
 begin
      DSize := strtoint(ComboBox2.Text);
      Button1.Height:=0;
-     Canvas.Pen.Color:=clFuchsia;
-     Canvas.Rectangle(50, 100, 600, 600);
-     Canvas.Pen.Color:=clBlack;
-     Canvas.Line(50, 350, 600, 350);
-     Canvas.Line(325, 100, 325, 600);
-     Canvas.Brush.color:=clBlack;
-      dot:= strtofloat(ComboBox1.Text) ;
-      min := strtofloat(Edit1.text);
-      max := strtofloat(Edit2.text);
-      x_len := (abs(min) + abs(max));
-      x_len := (x_len/dot);
-      x := min;
-      o := Trunc(dot);
-      canvas.brush.color:=clFuchsia     ;
-      canvas.pen.color:=clFuchsia        ;
+     with Form1.Image1.canvas do begin
+          Canvas.Pen.Color:=clBlack;
+       Brush.color:= clForm;
+       fillRect(0, 0, form1.image1.Width, form1.image1.Height);
+       //отрисовка осей
+       line(form1.image1.Width div 2, 0, form1.image1.Width div 2, form1.image1.Height);
+       line(0, form1.image1.Height div 2, form1.image1.Width, form1.image1.Height div 2);
+
+     dot:= strtofloat(ComboBox1.Text) ;
+     min := strtofloat(Edit1.text);
+     max := strtofloat(Edit2.text);
+     x_len := (abs(min) + abs(max));
+     x_len := (x_len/dot);
+     x := min;
+     o := Trunc(dot);
+      brush.color:=clFuchsia     ;
+      pen.color:=clFuchsia        ;
       for z:= 1 to o do
         begin
           y:= sin(x+2);
           convert();
-       //   if (x > Trunc(min)) or (x < Trunc(max))then
           begin
           if (100<=jy) or (jy>=600) then
           begin
-          canvas.Ellipse(ix, jy, (ix+DSize), (jy+DSize));
+          Ellipse(ix, jy, (ix+DSize), (jy+DSize));
           end;
           x := x + x_len;
          end;
         end;
-         canvas.pen.color:=clBlack;
-         canvas.brush.color:=clBlack;
+         pen.color:=clBlack;
+         brush.color:=clBlack;
       for i:= -10 to 10 do
         begin
           if i <> 0 then
         begin
-          ix := Trunc(Round((((i-min)*(600-50))/(max-min))+50));
-          jy := Trunc(Round((((0-min)*(600-100))/(min-max))+600));
-          Canvas.rectangle((ix-1),(jy-3),(ix+1),(jy+3));
-          ix := Trunc(Round((((0-min)*(600-50))/(max-min))+50));
-          jy := Trunc(Round((((i-min)*(600-100))/(min-max))+600));
-          Canvas.rectangle((ix-3),(jy-1),(ix+3),(jy+1));
+
+        //вывод еденичных отрезков по X
+        x:=i;
+        y:=0;
+        Convert();
+        Ellipsec(ix, jy, 3, 3);
+        //вывод еденичных отрезков по y
+        x:=0;
+        y:=i;
+        Convert();
+        Ellipsec(ix, jy, 3, 3);
+
         end;
       end;
+     end;
 
 end;
 
